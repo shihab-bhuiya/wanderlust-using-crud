a9ebe3487909fe9beca0b86a93e11451d23bb0df
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-05-13 20:15:06 +0600

    succesfully connect mongodb with backend server and create api using fronted data

---
+++ src/app/destinations/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DestinationsPage = () => {
+    return (
+        <div>
+            <h2>Destinations</h2>
+        </div>
+    );
+};
+
+export default DestinationsPage;
